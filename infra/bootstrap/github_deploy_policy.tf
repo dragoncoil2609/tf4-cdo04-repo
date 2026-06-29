989ff2bbd55692d5e8c7475802b9d2439d4021cb
dragoncoil2609 <hh26092004@gmail.com>
2026-06-29 19:19:12 +0700

    fix(bootstrap): add scheduler to untagged statement in deploy policy

--- infra/bootstrap/github_deploy_policy.tf
+++ infra/bootstrap/github_deploy_policy.tf
@@ -264,7 +264,8 @@ data "aws_iam_policy_document" "github_deploy_policy" {
       "ecs:DeregisterTaskDefinition",
       "cloudwatch:*",
       "sns:*",
-      "application-autoscaling:*"
+      "application-autoscaling:*",
+      "scheduler:*"
     ]
 
     resources = [
